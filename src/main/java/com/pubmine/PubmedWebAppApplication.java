4a8b8f0be2305a25f250cb861488c842f04499ed
Phearun Rath <rathphearun168@gmail.com>
2017-10-26 11:27:35 +0700

    added: search highlight

--- src/main/java/com/pubmine/PubmedWebAppApplication.java
+++ src/main/java/com/pubmine/PubmedWebAppApplication.java
@@ -2,15 +2,12 @@ package com.pubmine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import com.pubmine.repository.AbstractRepository;
 
 @SpringBootApplication
 public class PubmedWebAppApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(PubmedWebAppApplication.class, args);
+		/*ApplicationContext context = */SpringApplication.run(PubmedWebAppApplication.class, args);
 		/*
 		AbstractRepository repo = context.getBean(AbstractRepository.class);
 		repo.findOne(1279184).forEach(System.out::println);
